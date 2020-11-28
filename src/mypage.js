31db269aa4cbf448d83202167963906a25cd4e08
Elina Korhonen <69311167+elinakorhonen@users.noreply.github.com>
2020-11-28 20:01:10 +0200

    Initial commit

---
+++ src/mypage.js
@@ -0,0 +1,79 @@
+// home page, current date in every page
+
+// let today = new Date();
+
+//skills page
+
+//selectors
+// const addInput = document.querySelector(".add-input");
+// const addButton = document.querySelector(".add-button");
+// const skillsList = document.querySelector(".skills-list");
+
+//event
+// addButton.addEventListener("click", addSkill);
+
+//function
+// function addSkill(event) {
+// event.preventDefault();
+
+//const skillsDiv =document.createElement ("div");
+//skillsDiv.classList.add("skill");
+
+//const newSkill =document.createElement("li");
+// newSkill.innerText ="hey";
+//newSkill.classlist.add("skill-item");
+//newSkill.appenChild(newSkill);
+
+//delete button
+
+//const deleteButton = document.createElement("button");
+//deleteButton.innerHTML ="<i class="fas fa-trash-alt"></i>";
+//deleteButton.classList.add("")
+// skillsDiv.appenChild(deleteButton);
+
+//skillsList.appenChild(skillsDiv);
+// }
+
+// projects page
+
+//load event
+
+// let loaded = document.querySelector(".loaded");
+
+// window.addEventListener("load", (event) => {
+//   console.log("The page has fully loaded");
+// });
+
+//scroll event
+// let projects = document.querySelector(".projects");
+
+// window.addEventListener("scroll", (event) => {
+//   console.log("Scrolling");
+// });
+
+// mouse event
+
+//selector
+// let mouse = document.querySelector(".mouse");
+// console.log(mouse);
+
+//function
+// mouse.addEventListener("mouseover", function (event) {
+//   let target = event.target;
+//   target.style.color = "pink";
+// });
+
+//gallery
+
+// var opts = {
+//   auto: {
+//     speed: 3500,
+//     pauseOnHover: true,
+//   },
+//   fullScreen: false,
+//   swipe: true,
+// };
+// makeBSS(".photogallery", opts);
+
+const mouse = document.querySelector(".mouse");
+console.log(mouse);
